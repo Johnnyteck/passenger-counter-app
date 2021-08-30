b32d0504912c8fda2b7c4da1a746b5e2936a2188
Johnnyteck <73708569+Johnnyteck@users.noreply.github.com>
2021-08-30 14:13:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+// console.log(countEl)
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+   
+}
+
+function decrement() {
+    count = count - 1
+    countEl.innerText = count
+    if (count > 0) {
+        countEl.innerText = count
+    } else{
+        count = 0
+        countEl.innerText = count
+    }
+}
+
+function save() {
+    saveEl.innerText += " " + count + " - "
+}
