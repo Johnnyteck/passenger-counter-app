11244f035517ce17ea0f74970d966cc48cc8cffb
Johnnyteck <73708569+Johnnyteck@users.noreply.github.com>
2021-09-08 23:09:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 let count = 0
 let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+// let saveEl = document.getElementById("save-el")
+let saveEl = document.querySelector("#save-el")
 
-// console.log(countEl)
 
+
+// funtion to count number of passengers entering
 function increment() {
     count = count + 1
-    countEl.innerText = count
+    countEl.textContent = count
    
 }
 
+// funtion to decrement; incase someone entered and decided to illight again before the subway starts moving
 function decrement() {
     count = count - 1
-    countEl.innerText = count
+
     if (count > 0) {
-        countEl.innerText = count
+        countEl.textContent = count
     } else{
         count = 0
-        countEl.innerText = count
     }
 }
 
+// funtion to save the number of passengers that entered the subway
 function save() {
-    saveEl.innerText += " " + count + " - "
-    
+    if (count > 0) {
+        // saveEl.textContent += count + " - " 
+        saveEl.textContent += `${count} - `
+    } else {     
+    }
+
     count = 0
-    countEl.innerText = count
-}
+    countEl.textContent = count
+}   
